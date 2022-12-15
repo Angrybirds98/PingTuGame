858b5c9a6fe3897f507e284312dd4eb1b21d50c0
lqx <lqx@wudi.com>
2022-12-15 14:11:45 +0800

    first commit

---
+++ src/com/lqx/ui/de.java
@@ -0,0 +1,15 @@
+package com.lqx.ui;
+
+/**
+ * @program: PingTuGame
+ * @description:
+ * @author: LQX
+ * @created: 2022/12/15 11:31
+ */
+
+public class de {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+        System.out.println("哈哈2");
+    }
+}
